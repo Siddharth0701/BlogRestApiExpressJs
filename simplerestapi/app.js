09f7899aa036d07b696a277176200f66f69a2898
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-28 18:03:52 +0530

    Added Jwt

--- simplerestapi/app.js
+++ simplerestapi/app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();    
 const postRoute=require('./routes/posts');
+const userRoute=require('./routes/user');
 app.use(bodyParser.json());
 app.use("/posts",postRoute)
+app.use("/user",userRoute)
 module.exports=app
